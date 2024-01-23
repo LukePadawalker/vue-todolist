cde04926d98806dabf743cc77a561198069bfce1
LukePadawalker <padurariu.luca@gmail.com>
2024-01-23 17:34:04 +0100

    milestone 1 done

--- script.js
+++ script.js
@@ -3,13 +3,20 @@ console.log("Vue ok", Vue)
 const { createApp } = Vue;
 
 const app = createApp({
-    data() {
-        return tasks = [
+    data: () => ({
+        tasks: [
             { id: 1, done: false, text: 'Fare la spesa' },
             { id: 2, done: true, text: 'Lavare i capelli' },
             { id: 3, done: false, text: 'Comprare una marca da bollo' },
             { id: 4, done: false, text: 'Aggiornare il PC' }
         ]
+    }),
+    methods: {
+
     }
+
+
 })
 
+
+app.mount("#root");
