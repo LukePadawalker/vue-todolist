697a48c168e0b6136c46a9186d7f1b9abf28f8b2
LukePadawalker <padurariu.luca@gmail.com>
2024-01-23 16:14:54 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("Vue ok", Vue)
+
+const { createApp } = Vue;
+
+const app = createApp({
+    data() {
+        return tasks = [
+            { id: 1, done: false, text: 'Fare la spesa' },
+            { id: 2, done: true, text: 'Lavare i capelli' },
+            { id: 3, done: false, text: 'Comprare una marca da bollo' },
+            { id: 4, done: false, text: 'Aggiornare il PC' }
+        ]
+    }
+})
+
